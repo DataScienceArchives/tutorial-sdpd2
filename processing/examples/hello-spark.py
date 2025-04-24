14c850ef12223ea5d13aba140f5906034d6c1f92
Felipe Ortega <felipe.ortega@urjc.es>
2025-04-24 20:04:09 +0200

    Example files for Spark tutorial

---
+++ processing/examples/hello-spark.py
@@ -0,0 +1,27 @@
+from pyspark.sql import SparkSession
+import os
+
+def main():
+    os.environ["JAVA_HOME"]="/usr/lib/jvm/java-1.17.0-openjdk-amd64"
+    # Initialize SparkSession
+    spark = SparkSession.builder \
+        .appName("HelloWorld") \
+        .master("spark://localhost:7077") \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    
+
+    # Create an RDD containing numbers from 1 to 10
+    numbers_rdd = spark.sparkContext.parallelize(range(1, 1000))
+
+    # Count the elements in the RDD
+    count = numbers_rdd.count()
+
+    print(f"Count of numbers from 1 to 1000 is: {count}")
+
+    # Stop the SparkSession
+    spark.stop()
+
+
+if __name__ == "__main__":
+    main()
